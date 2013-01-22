27aa7c32e2d5a4de27ea877595cda5cf857341cd
Emile Kott <emilekott@gmail.com>
2013-01-22 17:21:45 +0000

    vimeo shortcode

--- wp-content/themes/skye/functions.php
+++ wp-content/themes/skye/functions.php
@@ -1,4 +1,28 @@
 <?php
-if ( function_exists( 'add_image_size' ) ) { 
-    
+include 'vimeo.php';
+
+function register_button( $buttons ) {
+   array_push( $buttons, "|", "vimeo" );
+   return $buttons;
 }
+
+function add_plugin( $plugin_array ) {
+   $plugin_array['vimeo'] = get_stylesheet_directory_uri() . '/js/vimeo.js';
+   return $plugin_array;
+}
+
+function my_vimeo_button() {
+
+   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') ) {
+      return;
+   }
+
+   if ( get_user_option('rich_editing') == 'true' ) {
+      add_filter( 'mce_external_plugins', 'add_plugin' );
+      add_filter( 'mce_buttons', 'register_button' );
+   }
+
+}
+
+add_action('init', 'my_vimeo_button');
+?>
---
+++ wp-content/themes/skye/vimeo.php
@@ -0,0 +1,23 @@
+<?php
+
+add_shortcode('vimeo', 'vimeo');
+
+function vimeo($atts) {
+    $atts = shortcode_atts(array(
+        'id' => '',
+        'width' => 848,
+        'height' => 477,
+        'desc' => "y",
+            ), $atts);
+
+    
+    if (isset($atts['id'])) {
+        $id = $atts['id'];
+        $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$id.php"));
+        $description = $hash[0]['description'];
+    }
+
+    $html = '<div class="videoContainer"><iframe src="http://player.vimeo.com/video/' . $atts['id'] . '?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ed1c24" width="'.$atts['width'].'" height="'.$atts['height'].'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>';
+    if ($atts['desc']=="y")   $html .= '<div class="video-description">'.$description."</div>";
+    return $html;
+}
