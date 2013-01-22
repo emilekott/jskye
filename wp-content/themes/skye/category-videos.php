29bbd15f40c94858ce27727fc307e2e3f1be4b26
Emile Kott <emilekott@gmail.com>
2013-01-22 23:12:57 +0000

    finishing touches

--- wp-content/themes/skye/category-videos.php
+++ wp-content/themes/skye/category-videos.php
@@ -12,21 +12,35 @@ get_header();
     <div id="primary" class="showcase">
         <div id="content" role="main">
             <h1 class="entry-title">Videos</h1>
-            <?php while (have_posts()) : the_post(); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <div class="video-thumb">
-                    <?php if (has_post_thumbnail()) { ?>
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('vimeo-thumb'); ?></a>
-                    <?php } else { ?>
-                        <a href="<?php the_permalink(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/missing-video.jpg" alt="News Item" /></a>
-                    <?php } ?>
-                    </div>
-                    <div class="video-title">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </div>
-                </article><!-- #post-<?php the_ID(); ?> -->
-            <?php endwhile; // end of the loop. ?>
+            <div class="entry-content">
+                <?php while (have_posts()) : the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="video-thumb">
+                            
+                                <?php
+                                $filename = 'wp-content/uploads/video_thumb_' . get_the_ID() . '.jpg';
+                                $upload_dir = wp_upload_dir();
+                                $url = $upload_dir['baseurl'].'/video_thumb_' . get_the_ID() . '.jpg';
+                                if (!file_exists($filename)) {
+                                    $image = wp_get_image_editor(get_video_thumbnail()); // Return an implementation that extends <tt>WP_Image_Editor</tt>
 
+                                    if (!is_wp_error($image)) {
+                                        $image->resize(406, 232, true);
+                                        $image->save($filename);
+                                    }
+                                }
+                                ?>
+
+
+                                <a href="<?php the_permalink(); ?>"><img src="<?php echo $url; ?>"</a>
+                            
+                        </div>
+                        <div class="video-title">
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        </div>
+                    </article><!-- #post-<?php the_ID(); ?> -->
+                <?php endwhile; // end of the loop.   ?>
+            </div>
         </div><!-- #content -->
     </div><!-- #primary -->
 </div>
